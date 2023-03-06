93300f342aaa12704357dc5c73b525f4efb00f0c
georgiy <georgiy99@bk.ru>
2023-03-06 22:17:42 +0300

    bugfix

--- grb/config.py
+++ grb/config.py
@@ -4,21 +4,21 @@ import os
 ROOT_PATH = './'
 
 IMAGE_PATH = f'{ROOT_PATH}pics/'
-if not os.parh.exists(IMAGE_PATH):
+if not os.path.exists(IMAGE_PATH):
     os.makedirs(IMAGE_PATH)
 
 LIGHT_CURVE_SAVE = f'{ROOT_PATH}light_curves/'
-if not os.parh.exists(LIGHT_CURVE_SAVE):
+if not os.path.exists(LIGHT_CURVE_SAVE):
     os.makedirs(LIGHT_CURVE_SAVE)
 
 ACS_DATA_PATH = 'E:/ACS/'
 
 DATA_PATH = f'{ROOT_PATH}data/'
-if not os.parh.exists(DATA_PATH):
+if not os.path.exists(DATA_PATH):
     os.makedirs(DATA_PATH)
 
 LOGS_PATH = f'{ROOT_PATH}logs/'
-if not os.parh.exists(LOGS_PATH):
+if not os.path.exists(LOGS_PATH):
     os.makedirs(LOGS_PATH)
 
 GBM_DETECTOR_CODES = {0:'n0',1:'n1',2:'n2',3:'n3',4:'n4',5:'n5',6:'n6',7:'n7',8:'n8',9:'n9',10:'na',11:'nb',12:'b0',13:'b1'}
